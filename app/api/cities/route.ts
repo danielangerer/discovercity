e8ff867964c54297d17448ec1ea5d1794685ee25
danielangerer <daniel00angerer@gmail.com>
2025-07-18 14:39:35 +0200

    Add comment to clarify database usage in GET cities function

--- app/api/cities/route.ts
+++ app/api/cities/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server';
 import { getCities } from '@/lib/getCities';
 
 export async function GET() {
+    //await only necessary if real db is used
   const cities = await getCities();
   return NextResponse.json(cities);
 }
