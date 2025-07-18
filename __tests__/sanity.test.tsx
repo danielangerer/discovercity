9be77b51e1f489ed98d20d3556af6ebfb885fc9d
danielangerer <daniel00angerer@gmail.com>
2025-07-18 17:44:03 +0200

    Refactor: update Jest configuration and setup for improved testing compatibility

--- __tests__/sanity.test.tsx
+++ __tests__/sanity.test.tsx
@@ -1,14 +1,10 @@
-/**
- * @jest-environment jsdom
- */
-import React from 'react'
 import { render, screen } from '@testing-library/react'
 
 const Dummy = () => <div data-testid="dummy">Hello Dummy</div>
 
-describe('Sanity test: Test setup works', () => {
+describe('Sanity test', () => {
   it('renders dummy text', () => {
     render(<Dummy />)
-    expect(screen.getByTestId('dummy').textContent).toBe('Hello Dummy')
+    expect(screen.getByTestId('dummy')).toHaveTextContent('Hello Dummy')
   })
 })
---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import { JestConfigWithTsJest } from 'ts-jest'
+
+const config: JestConfigWithTsJest = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+}
+
+export default config
